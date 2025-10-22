16a16dc1d1366f56fd43fccf460aa6ac0a0da072
jn134447 <196645821+jn134447@users.noreply.github.com>
2025-10-22 23:45:53 +0200

    Finished the rest of the exercise

--- vjezba_02/src/03.cpp
+++ vjezba_02/src/03.cpp
@@ -24,19 +24,15 @@ Matrix user_defined_matrix(int row, int column) {
   return matrix;
 }
 
-// biggest BS in the task, the `end`(b) param is legit only for error checking,
-// it is NOT used as an "end" to the sequence as a matrix already has limited
-// space, meaning any overflow from range is never accounted for, making `end`
-// useless as the sequence ends when the matrix ends.
-// The requirements are insanely vague anyways, this [a,b] could mean anything.
-// i'm assuming its a range of natural numbers
+// definition of range for this function is too vague
 Matrix range_defined_matrix(int row, int column, int start, int end) {
   Matrix matrix = create_empty_matrix(row, column);
 
   int range_len = end - start + 2;
   if (range_len <= (matrix.m * matrix.n)) {
-    cout << "error: range(" << start << ", " << end
-         << ") is too small to cover the entire matrix " << endl;
+    cout << "error: range[" << start << ", " << end
+         << "] is too small to cover the entire matrix of "
+         << (matrix.m * matrix.n) << endl;
     return matrix;
   }
   if (start > end) {
@@ -48,6 +44,16 @@ Matrix range_defined_matrix(int row, int column, int start, int end) {
     for (int j = 0; j < matrix.n; j++)
       matrix.elements[i][j] = start++;
   return matrix;
+}
+
+Matrix transpose_matrix(Matrix &A) {
+  Matrix A_t = create_empty_matrix(A.n, A.m);
+  for (int i = 0; i < A_t.m; i++) {
+    for (int j = 0; j < A_t.n; j++) {
+      A_t.elements[i][j] = A.elements[j][i];
+    }
+  }
+  return A_t;
 }
 
 Matrix operate_matrices(Matrix &A, Matrix &B, char op) {
@@ -69,18 +75,51 @@ Matrix operate_matrices(Matrix &A, Matrix &B, char op) {
   }
   return C;
 }
+
 Matrix add_matrices(Matrix &A, Matrix &B) {
   return operate_matrices(A, B, '+');
 }
+
 Matrix subtract_matrices(Matrix &A, Matrix &B) {
   return operate_matrices(A, B, '-');
 }
 
-void print_matrix(Matrix &matrix) {
+/* WIKIPEDIA:
+ *  Input: matrices A[n*m] and B[m*p]
+ *  Let C[n*p] be a new matrix of the appropriate size
+ *  For i from 1 to n:
+ *      For j from 1 to p:
+ *          Let sum = 0
+ *          For k from 1 to m:
+ *              Set sum ← sum + Aik × Bkj
+ *          Set Cij ← sum
+ *  Return C
+ */
+Matrix multiply_matrices(Matrix &A, Matrix &B) {
+  if (A.n != B.m) {
+    cout << "error: cant multiply incompatable matrices" << endl;
+    return Matrix{nullptr, 0, 0};
+  }
+
+  Matrix C = create_empty_matrix(A.m, B.n);
+  int sum;
+  for (int i = 0; i < C.m; i++) {
+    for (int j = 0; j < C.n; j++) {
+      sum = 0;
+      for (int k = 0; k < B.m; k++) // or A.n
+        sum = sum + A.elements[i][k] * B.elements[k][j];
+      C.elements[i][j] = sum;
+    }
+  }
+
+  return C;
+}
+
+void print_matrix(Matrix &matrix, int width = 10) {
   cout << endl;
   for (int i = 0; i < matrix.m; i++) {
     for (int j = 0; j < matrix.n; j++) {
-      cout << right << setw(9) << fixed << setprecision(4)
+      cout << right << setw(width) << fixed << setprecision(4)
            << matrix.elements[i][j];
     }
     cout << endl;
@@ -99,17 +138,37 @@ int main() {
   Matrix matrix_a = user_defined_matrix(m, n);
   Matrix matrix_b = range_defined_matrix(3, 2, 1, 6);
   Matrix matrix_c = range_defined_matrix(3, 2, -6, 0);
-  Matrix matrix_d = range_defined_matrix(3, 2, 1, 6);
+  Matrix matrix_d = range_defined_matrix(5, 2, 1, 10);
 
+  cout << "A: ";
   print_matrix(matrix_a);
+  cout << "B: ";
   print_matrix(matrix_b);
+  cout << "C: ";
   print_matrix(matrix_c);
+  cout << "D: ";
   print_matrix(matrix_d);
 
-  Matrix matrix_c_add_d = add_matrices(matrix_c, matrix_d);
-  Matrix matrix_c_sub_d = subtract_matrices(matrix_c, matrix_d);
+  Matrix matrix_add_exmpl = add_matrices(matrix_b, matrix_c);
+  if (!matrix_add_exmpl.elements)
+    return 1;
 
-  cout << "add/sub: ";
-  print_matrix(matrix_c_add_d);
-  print_matrix(matrix_c_sub_d);
+  Matrix matrix_sub_exmpl = subtract_matrices(matrix_b, matrix_c);
+  if (!matrix_sub_exmpl.elements)
+    return 2;
+
+  Matrix matrix_d_transposed = transpose_matrix(matrix_d);
+
+  Matrix matrix_mul_exmpl = multiply_matrices(matrix_c, matrix_d_transposed);
+  if (!matrix_mul_exmpl.elements)
+    return 3;
+
+  cout << "add(B+C): ";
+  print_matrix(matrix_add_exmpl);
+  cout << "sub(B-C): ";
+  print_matrix(matrix_sub_exmpl);
+  cout << "D_transposed:";
+  print_matrix(matrix_d_transposed);
+  cout << "mul(D[5x2]*C[3x2]): ";
+  print_matrix(matrix_mul_exmpl);
 }
