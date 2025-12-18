c116a49e07437b8f96c43c012b5c2fc5e089af37
jn134447 <196645821+jn134447@users.noreply.github.com>
2025-12-18 15:52:07 +0100

    vj_08 first iteration of the implementations

--- vjezba_08/include/game.hpp
+++ vjezba_08/include/game.hpp
@@ -2,36 +2,120 @@
 #define GAME_H
 
 #include <string>
+#include <iostream>
 
-enum class Difficulty
-{
-    EASY,
-    MEDIUM,
-    HARD,
-    INSANE
-};
+constexpr int HEALTH_DEATH_THRESHOLD = 0;
+constexpr int SCORE_ON_KILL = 10;
+
+constexpr int WARRIOR_INIT_HP = 120;
+constexpr int MAGE_INIT_HP = 80;
+constexpr int GNOME_INIT_HP = 90;
+constexpr int BOSS_INIT_HP = 300;
+
+constexpr int GNOME_INIT_DIFFICULTY = 3;
+constexpr int BOSS_INIT_DIFFICULTY = 8;
+
+constexpr int MAGE_SP_HEALTH_THRESHOLD = 50;
+constexpr int GNOME_SP_HEALTH_THRESHOLD = 20;
+constexpr int BOSS_MAX_HP_THRESHOLD = 300;
 
 class GameCharacter
 {
 protected:
     int health;
     std::string name;
+    bool dead = false;
+
+    // pretty sure this is what they call an interface
+    virtual void specialAbility() = 0;
+
+public:
+    GameCharacter(int health, std::string name);
+    virtual void displayStatus() const;
+
+    virtual void takeDamage(int amount);
+    virtual bool isAlive() const;
 };
 
 class Player : public GameCharacter
 {
 protected:
     int score;
+
+    void addScore(int amount);
+
+    virtual void attackEnemy(Enemy &enemy) = 0;
+
+public:
+    Player(int health, std::string name);
 };
 
 class Enemy : public GameCharacter
 {
 protected:
-    Difficulty difficulty;
+    unsigned int difficulty;
+
+    virtual void attackPlayer(Player &player) = 0;
+
+public:
+    Enemy(int health, std::string name, unsigned int difficulty);
 };
 
 // Player classes
+class Warrior : public Player
+{
+protected:
+    bool shieldActive = false;
+    float shieldEffectiveness = 0.5;
+    int attackDamage = 20;
+
+public:
+    Warrior(std::string name);
+
+    void attackEnemy(Enemy &enemy) override;
+    void takeDamage(int amount) override;
+    void specialAbility() override;
+};
+
+class Mage : public Player
+{
+protected:
+    int mana = 100;
+
+    int attackDamageFireball = 40;
+    int manaCostFireball = 20;
+    int attackDamageStick = 20;
+
+    Mage(std::string name);
+
+public:
+    void attackEnemy(Enemy &enemy) override;
+    void specialAbility() override;
+};
 
 // Enemy classes
+class Gnome : public Enemy
+{
+protected:
+    int attackDamage = 5;
+
+public:
+    Gnome(std::string name);
+
+    virtual void attackPlayer(Player &player) override;
+    void specialAbility() override;
+};
+
+class Boss : public Enemy
+{
+protected:
+    int attackDamage = 10;
+    int healthRegen = 50;
+public:
+    Boss(std::string name);
+
+    virtual void attackPlayer(Player &player) override;
+    void specialAbility() override;
+};
 
 #endif
---
+++ vjezba_08/src/game.cpp
@@ -0,0 +1,175 @@
+#include "game.hpp"
+
+GameCharacter::GameCharacter(int health, std::string name)
+    : health(health),
+      name(name)
+{
+}
+
+void GameCharacter::displayStatus() const
+{
+    std::cout << "displayStatus(): "
+              << health << '\t'
+              << name << '\t'
+              << dead << '\n';
+}
+
+void GameCharacter::takeDamage(int amount)
+{
+    health -= amount;
+    std::cout << name << "> took " << amount << " damage [" << health << "]" << '\n';
+
+    if (health < HEALTH_DEATH_THRESHOLD)
+        dead = true;
+}
+
+bool GameCharacter::isAlive() const
+{
+    return !dead;
+}
+
+void Player::addScore(int amount)
+{
+    score += amount;
+    std::cout << name << "> earned " << amount << " points [" << score << "]" << '\n';
+}
+
+Player::Player(int health, std::string name)
+    : GameCharacter(health, name)
+{
+}
+
+Enemy::Enemy(int health, std::string name, unsigned int difficulty)
+    : GameCharacter(health, name),
+      difficulty(difficulty)
+{
+}
+
+Warrior::Warrior(std::string name) : Player(WARRIOR_INIT_HP, name)
+{
+}
+
+void Warrior::attackEnemy(Enemy &enemy)
+{
+    if (!enemy.isAlive())
+        return;
+
+    std::cout << name << "> slashes with his sword" << '\n';
+    enemy.takeDamage(attackDamage);
+
+    // if killed
+    if (!enemy.isAlive())
+    {
+        addScore(SCORE_ON_KILL);
+    }
+}
+
+void Warrior::takeDamage(int amount)
+{
+    if (shieldActive)
+    {
+        amount = amount * shieldEffectiveness;
+        shieldActive = false;
+    }
+
+    health -= amount;
+    std::cout << name << "> took " << amount << " damage [" << health << "]" << '\n';
+
+    if (health < HEALTH_DEATH_THRESHOLD)
+    {
+        dead = true;
+        shieldActive = false;
+    }
+}
+
+void Warrior::specialAbility()
+{
+
+    std::cout << name << "> is guarding" << '\n';
+    shieldActive = true;
+}
+
+Mage::Mage(std::string name) : Player(MAGE_INIT_HP, name)
+{
+}
+
+void Mage::attackEnemy(Enemy &enemy)
+{
+    if (!enemy.isAlive())
+        return;
+
+    if (mana >= manaCostFireball)
+    {
+        mana -= manaCostFireball;
+        std::cout << name << "> used fireball" << '\n';
+        enemy.takeDamage(attackDamageFireball);
+    }
+    else
+    {
+        std::cout << name << "> used his stick to attack" << '\n';
+        enemy.takeDamage(attackDamageStick);
+    }
+
+    // if killed
+    if (!enemy.isAlive())
+    {
+        addScore(SCORE_ON_KILL);
+    }
+}
+
+void Mage::specialAbility()
+{
+    if (health < MAGE_SP_HEALTH_THRESHOLD)
+    {
+        std::cout << name << "> insufficent amount of health for a teleport" << '\n';
+    }
+    std::cout << name << "> teleporting..." << '\n';
+}
+
+Gnome::Gnome(std::string name) : Enemy(GNOME_INIT_HP, name, GNOME_INIT_DIFFICULTY)
+{
+}
+
+void Gnome::attackPlayer(Player &player)
+{
+    if (!player.isAlive())
+        return;
+
+    std::cout << name << "> is attacking" << '\n';
+    player.takeDamage(attackDamage * difficulty);
+}
+
+void Gnome::specialAbility()
+{
+    if (health > GNOME_SP_HEALTH_THRESHOLD)
+    {
+        std::cout << name << "> used the horn." << '\n';
+    }
+    std::cout << name << "> too weak to use the horn." << '\n';
+}
+
+Boss::Boss(std::string name) : Enemy(BOSS_INIT_HP, name, BOSS_INIT_DIFFICULTY)
+{
+}
+
+void Boss::attackPlayer(Player &player)
+{
+    if (!player.isAlive())
+        return;
+
+    std::cout << name << "> is attacking, watch out!" << '\n';
+    player.takeDamage(attackDamage * difficulty);
+}
+
+void Boss::specialAbility()
+{
+    if (health + healthRegen >= BOSS_MAX_HP_THRESHOLD)
+    {
+        health = BOSS_MAX_HP_THRESHOLD;
+    }
+    else
+    {
+        health += healthRegen;
+    }
+    std::cout << name << "> used regeneration, now has " << health << " health" << '\n';
+}
